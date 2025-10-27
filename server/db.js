102252ea54dd5408d82421f913809fc8e18a87be
Madison Duran <madisonduran74@gmail.com>
2025-10-27 17:15:32 -0400

    Installed mysql and popluated 2 json files, created a env file, worked on db.js to connect the database.

--- server/db.js
+++ server/db.js
@@ -1,1 +1,12 @@
-// database file
+const mysql = require('mysql2/promise');
+
+const pool = mysql.createPool({
+host: 'localhost',
+user: 'root',
+password: 'password', 
+database: 'Plant-Pal', 
+port: 3306,
+connectionLimit: 10
+});
+
+module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -26,3 +26,5 @@ app.listen(PORT, async () => {
     console.log(`Server running on http://localhost:${PORT}`);
     await verifyDatabaseConnection();
 });
+
+
